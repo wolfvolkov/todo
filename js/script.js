cf1453e0164ceb40066b640bad96339b88762d68
wolf <kirill@yargromov.com>
2013-11-17 19:37:53 +0200

    final for today functionality

--- js/script.js
+++ js/script.js
@@ -54,7 +54,8 @@ ObjectStorage.prototype = {
 
 var storage = new ObjectStorage;
 var tasks = new Object();
-console.log(storage.local.t1);
+var subTid = 0;
+
 if(storage.local.t1){
 	tasks = storage.local;
 }else{
@@ -62,72 +63,107 @@ if(storage.local.t1){
 		                        date : new Date(),
 		                        name : "Создать новые задачи",
 		                        id : 0,
-		                        state : false
+		                        state : false,
+		                        subtasks : {}
 		                      }; 
 	}
 
 var tId = 0;
-
-for(var i in tasks){
-	tId = tasks[i].id+1;
-
-} 
-
-
 Drow();
 function Drow(){
-    // 
-    for(var i in tasks){
-        tId = tasks[i].id+1;
-    }   
-    $('.panel-group').empty();
-    var html = "";
-    for(var i in tasks){
-
-
-        // console.log(i +", "+ tasks[i].id);
-        html+= '<div class="panel panel-default" id = "t'+tasks[i].id+'">';
-        html+= '<div class="panel-heading row">';
-        html+= '<h4 class="panel-title col-lg-8">';
-        html+= '<a data-toggle="collapse" data-parent="#accordion" href="#collapse'+tasks[i].id+'" class="collapsed">'+tasks[i].name+'</a>';
-        html+= '</h4>';
-        html+= '<div class="task_control pull-right col-lg-4">';
-        html+= '<button type="button" class="btn btn-success">Success</button>';
-        html+= '</div>';
-        html+= '</div>';
-        html+= '<div id="collapse'+tasks[i].id+'" class="panel-collapse collapse"><div class="panel-body">';
-        html+= '';
-        html+= '</div></div>';
-        html+= '</div>'; 
-        if(tasks[i].state === true){
-        $('#compliteTasks .panel-group').append(html);
-    	}
-	    else{
-	        $('#newTasks .panel-group').append(html);
-	    }  
-	    html = "";     
-    }   
-
-    
-
-    controls();
+	console.log(tId); 
+	$('.panel-group').empty();
+	var html = "";
+	for(var i in tasks){
+		html+= '<div class="panel panel-default" id = "t'+tasks[i].id+'">';
+		html+= '<div class="panel-heading row">';
+		html+= '<h4 class="panel-title col-lg-8">';
+		html+= '<a data-toggle="collapse" data-parent="#accordion" href="#collapse'+tasks[i].id+'" class="collapsed">'+tasks[i].name+'</a>';
+		html+= '</h4>';
+		html+= '<div class="task_control pull-right col-lg-4">';
+		html+= '<button  type="button" class="btn btn-success"><span class="glyphicon glyphicon-ok glyphicon-white"></span></button>';
+		html+= '</div>';
+		html+= '</div>';
+		if(!$.isEmptyObject(tasks[i].subtasks)){
+			html+= '<div id="collapse'+tasks[i].id+'" class="panel-collapse collapse"><div class="panel-body"><ul>';
+			for(var j in tasks[i].subtasks){
+				html+= '<li>'+tasks[i].subtasks[j]+'</li>';
+			}
+			html+= '</ul></div>';
+		}
+		html+= '</div>';
+		html+= '</div>'; 
+		if(tasks[i].state === true){
+			$('#compliteTasks .panel-group').append(html);
+		}
+		else{
+			$('#newTasks .panel-group').append(html);
+		}  
+		html = "";     
+	}     
+	controls();
 }
-
-$('#addButton').click(function(){
-
+$('#taskNameAdd').bind('change click keyup',function(){
+	if($('#taskNameAdd').val().length > 0){
+		$('#addButton').removeAttr("disabled");
+	}
+	else{
+		$('#addButton').attr("disabled","disabled");
+	}
+});
+$('#taskName').bind('change click keyup',function(){
+	if($('#taskName').val().length > 0){
+		$('.modal-footer button').removeAttr("disabled");
+	}
+	else{
+		$('.modal-footer button').attr("disabled","disabled");
+	}
+});
+$('#subTaskNameAdd').bind('change click keyup',function(){
+	if($('#subTaskNameAdd').val().length > 0){
+		$('#addSubButton').removeAttr("disabled");
+	}
+	else{
+		$('#addSubButton').attr("disabled","disabled");
+	}
+});
+function DrowModal(){
+	$('.modal-body ul').append("<li id = '"+subTid+"'>"+tasks["t"+(tId-1)].subtasks[subTid]+"</li>");
+	console.log(tasks["t"+(tId-1)].subtasks);
+		subTid+=1;
+}
+function addTask(){
     tasks["t"+tId] = {
                         date : new Date(),
                         name : $('#taskNameAdd').val(),
                         id : tId,
-                        state : false
-                      };
-
+                        state : false,
+                        subtasks : {}
+                      };    
     
-    $('#taskNameAdd').val(" ");
     storage.local = tasks;
+    for(var i in tasks){
+    	tId = tasks[i].id+1;
+    }  
     Drow();
+}
+$('#addButton').on('click',function(){
+	addTask();
+	if($('form .checkbox input').prop('checked')){
+		$('#task_settings form #taskName').val($('#taskNameAdd').val());
+		$('#task_settings').fadeIn();
+	}
+	else{
+		$('#taskNameAdd').val(" ");
+	}
 });
+$('#addSubButton').on('click',function(){
+	tasks["t"+(tId-1)].subtasks[subTid] = $('#subTaskNameAdd').val();
+	
+	$('#subTaskNameAdd').val("")
+	DrowModal();
 
+});
 function controls(){
 	$('.btn-success').on('click', function(){
 		var panel = $(this).parents('.panel');
@@ -137,10 +173,15 @@ function controls(){
 		Drow();
 	});
 }
-$('#myTab li').click(Drow());
 
-
+$('.modal-footer button').on('click',function(){
+	tasks["t"+(tId-1)].name = $('#task_settings form #taskName').val();
+	$('#taskNameAdd').val(" ");
+	$('#task_settings').fadeOut();
+	console.log(tasks["t"+(tId-1)]);
+	Drow();
+	subTid = 0;
+});
+// $('#myTab li').click(Drow());
 $('.content_wrapper').css('padding-bottom',$('footer').height());
-
-
 storage.local = {};
